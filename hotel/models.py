7eee4e88e097d10a9f497e46e89914f0112a54c1
Mel B <imelda52602@gmail.com>
2023-11-05 15:53:03 +0000

    made room model
    filler images currently used. to be changed. filler room objects created

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    ROOM_TYPES = (
+        ('SINGLE', 'Single'),
+        ('DOUBLE', 'Double'),
+        ('FAMILY', 'Family'),
+    )
+            
+    room_number = models.IntegerField()
+    room_type = models.CharField(max_length=20, choices=ROOM_TYPES)
+    room_beds = models.IntegerField()
+    room_capacity = models.IntegerField()
+    room_price = models.IntegerField()
+    room_description = models.TextField()
+    room_image = models.ImageField(upload_to='static/images')
+
+    def __str__(self):
+        return f'{self.room_number}: {self.room_type} with {self.room_beds} beds for {self.room_capacity} people'
