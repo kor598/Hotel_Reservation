b8d26becc1b70545246c2a258372a05f00b1f595
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-11-13 04:40:21 +0000

    fixing imports and response

--- payments/views.py
+++ payments/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from payment_processors import PaymentProcessorFactory
+from .payment_processors.payment_processor_factory import PaymentProcessorFactory
+
 
 def process_payment(request, processor_type):
+
+    #order_id = request.session.get('order_id')
+    #order = get_object_or_404(Order, id=order_id)
+
     # Create a Payment Processor depending on the processor type  
     factory = PaymentProcessorFactory()
     processor = factory.create_payment_processor(processor_type)
 
     # Process the Payment
-    processor.processPayment()
+    response = processor.processPayment(request)
 
     # Return response to the client
-    return HttpResponse(f"Payment processed with {processor_type}")
+    return response
