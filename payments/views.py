f1b26e96d77eb863506770923ece056ac537cac5
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-11-13 02:03:16 +0000

    Add payment processing view

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from payment_processors import PaymentProcessorFactory
 
-# Create your views here.
+def process_payment(request, processor_type):
+    # Create a Payment Processor depending on the processor type  
+    factory = PaymentProcessorFactory()
+    processor = factory.create_payment_processor(processor_type)
+
+    # Process the Payment
+    processor.processPayment()
+
+    # Return response to the client
+    return HttpResponse(f"Payment processed with {processor_type}")
