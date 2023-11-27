81ffd0329bc44651f3373c8ee3cd3b276d1383cd
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-11-27 17:23:39 +0000

    Add payment paths for paypal cancel and return

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import process_payment
+from .payment_processors.paypal_payment_processor import PaypalPaymentProcessor
+from .payment_processors.payment_processor_factory import PaymentProcessorFactory
+from .payment_processors.stripe_payment_processor import StripePaymentProcessor
 
 urlpatterns = [
-    path('process_payment/<str:processor_type>/', process_payment, name='process_payment'),
+    path('process_payment/<str:processor_type>/', process_payment, name='paypal-ipn'),
+    path('paypal_return/',process_payment, name='payment-return'),
+    path('paypal_cancel/',process_payment, name='payment-cancelled'),
 ]
