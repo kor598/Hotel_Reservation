3c31688db6a34b9bc9fe9f0283af2be1128e614b
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-11-29 16:12:52 +0000

    Change factory pattern in views.py to strategy

--- payments/views.py
+++ payments/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .payment_processors.payment_processor_factory import PaymentProcessorFactory
+from django.shortcuts import render
+from payment_processors.paypal_payment_processor import PaypalPaymentProcessor
+from payment_processors.stripe_payment_processor import StripePaymentProcessor
 
+def get_payment_strategy(processor_type):
+
+    # payment strategies available
+    payment_strategies = {
+        'paypal': PaypalPaymentProcessor,
+        'stripe': StripePaymentProcessor,
+    }
+
+    return payment_strategies.get(processor_type)
 
 def process_payment(request, processor_type):
 
-    #order_id = request.session.get('order_id')
-    #order = get_object_or_404(Order, id=order_id)
+    # get the payment strategy selected
+    payment_strategy = get_payment_strategy(processor_type)
+    
+    if payment_strategy is None:
+        # Handle unsupported payment method
+        return render(request, 'error_template.html', {'error_message': 'Unsupported payment method'})
 
-    # Create a Payment Processor depending on the processor type  
-    factory = PaymentProcessorFactory()
-    processor = factory.create_payment_processor(processor_type)
+    payment_processor = payment_strategy()
+    payment_result = payment_processor.process_payment(request)
 
-    # Process the Payment
-    response = processor.processPayment(request)
-
-    # Return response to the client
-    return response
+    if payment_result == 'success':
+        return payment_processor.handle_success(request)
+    else:
+        return payment_processor.handle_failure(request)
