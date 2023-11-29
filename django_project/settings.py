98cab28ea171fb2bd619218abc0536e2a67cca18
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-11-29 23:21:45 +0000

    Allow hosts for IPN testing

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-91d3fnnbg471h#n0xkwc(46#r#9%f7a=4l+rrhux0a+-227-+_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
