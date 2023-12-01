8e77156efb70a8a67b3ade0c3a5ae38266fb24ab
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 17:21:30 +0000

    Remove unecessary if statement for payment status

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.shortcuts import render
-from payment_processors.paypal_payment_processor import PaypalPaymentProcessor
-from payment_processors.stripe_payment_processor import StripePaymentProcessor
+from .payment_processors.paypal_payment_processor import PaypalPaymentProcessor
+from .payment_processors.stripe_payment_processor import StripePaymentProcessor
 
 def get_payment_strategy(processor_type):
 
@@ -18,15 +17,13 @@ def process_payment(request, processor_type):
 
     # get the payment strategy selected
     payment_strategy = get_payment_strategy(processor_type)
-    
-    if payment_strategy is None:
-        # Handle unsupported payment method
-        return render(request, 'error_template.html', {'error_message': 'Unsupported payment method'})
 
+    # instantiate the strategy
     payment_processor = payment_strategy()
-    payment_result = payment_processor.process_payment(request)
 
-    if payment_result == 'success':
-        return payment_processor.handle_success(request)
-    else:
-        return payment_processor.handle_failure(request)
+    if payment_strategy:
+        payment_processor_instance = payment_strategy()
+        return payment_processor_instance.process_payment(request)
+
+    # Handle unsupported payment method
+    return render(request, 'error.html', {'error_message': 'Unsupported payment method'})
