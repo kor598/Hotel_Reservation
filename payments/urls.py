3ef54fc9fd1f209a35da2080e030dcf0d777189a
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 18:45:15 +0000

    Update payment URLs with booking id

--- payments/urls.py
+++ payments/urls.py
@@ -3,16 +3,17 @@ from .views import process_payment
 from .payment_processors.paypal_payment_processor import PaypalPaymentProcessor
 from .payment_processors.stripe_payment_processor import StripePaymentProcessor
 
+
 # unique url  name for each payment success or failure
 urlpatterns = [
     # Paypal urls
-    path('payment/paypal/', PaypalPaymentProcessor.process_payment),
-    path('payment-success/paypal/', PaypalPaymentProcessor.payment_success, name='paypal-return'),
-    path('payment-cancelled/paypal/', PaypalPaymentProcessor.payment_failure, name='paypal-cancel'),
+    path('payment/paypal/<int:booking_id>/', PaypalPaymentProcessor.as_view(), name='paypal-payment'),    
+    path('payment-success/paypal/', PaypalPaymentProcessor.as_view(), name='paypal-return'),
+    path('payment-cancelled/paypal/', PaypalPaymentProcessor.as_view(), name='paypal-cancel'),
 
     # Stripe urls
-    path('payment/stripe/', StripePaymentProcessor.process_payment),
-    path('payment-success/stripe/', StripePaymentProcessor.payment_success, name='stripe-return'),
-    path('payment-cancelled/stripe/', StripePaymentProcessor.payment_failure, name='stripe-cancel'),
+    path('payment/stripe/<int:booking_id>/', StripePaymentProcessor),
+    path('payment-success/stripe/', StripePaymentProcessor, name='stripe-return'),
+    path('payment-cancelled/stripe/', StripePaymentProcessor, name='stripe-cancel'),
     # path('stripe_webhook/', StripePaymentProcessor.stripe_webhook, name='stripe-webhook')
 ]
