19f1c80e7db02334992a2d4d317a081bff6426d1
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 17:18:38 +0000

    Add unique urls

--- payments/urls.py
+++ payments/urls.py
@@ -3,9 +3,16 @@ from .views import process_payment
 from .payment_processors.paypal_payment_processor import PaypalPaymentProcessor
 from .payment_processors.stripe_payment_processor import StripePaymentProcessor
 
-
+# unique url  name for each payment success or failure
 urlpatterns = [
-    path('payment/', PaypalPaymentProcessor.process_payment),
-    path('payment-success/', PaypalPaymentProcessor.payment_success, name='paypal-return'),
-    path('payment-cancelled/', PaypalPaymentProcessor.payment_failure, name='paypal-cancel'),
+    # Paypal urls
+    path('payment/paypal/', PaypalPaymentProcessor.process_payment),
+    path('payment-success/paypal/', PaypalPaymentProcessor.payment_success, name='paypal-return'),
+    path('payment-cancelled/paypal/', PaypalPaymentProcessor.payment_failure, name='paypal-cancel'),
+
+    # Stripe urls
+    path('payment/stripe/', StripePaymentProcessor.process_payment),
+    path('payment-success/stripe/', StripePaymentProcessor.payment_success, name='stripe-return'),
+    path('payment-cancelled/stripe/', StripePaymentProcessor.payment_failure, name='stripe-cancel'),
+    # path('stripe_webhook/', StripePaymentProcessor.stripe_webhook, name='stripe-webhook')
 ]
