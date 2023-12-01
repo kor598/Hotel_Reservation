20eff8fb66c99e09daaf1f365f126e2ed9717abd
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 18:45:36 +0000

    added booking_id to views

--- payments/views.py
+++ payments/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .payment_processors.paypal_payment_processor import PaypalPaymentProcessor
 from .payment_processors.stripe_payment_processor import StripePaymentProcessor
+from bookings.models import Booking
 
 def get_payment_strategy(processor_type):
 
@@ -13,17 +14,18 @@ def get_payment_strategy(processor_type):
 
     return payment_strategies.get(processor_type)
 
-def process_payment(request, processor_type):
+def process_payment(request, processor_type, booking_id):
+    # Retrieve booking details based on booking_id
+    booking = get_object_or_404(Booking, id=booking_id)
 
-    # get the payment strategy selected
+    # Get the payment strategy selected
     payment_strategy = get_payment_strategy(processor_type)
 
-    # instantiate the strategy
-    payment_processor = payment_strategy()
-
+    # Check if the payment strategy is available
     if payment_strategy:
-        payment_processor_instance = payment_strategy()
-        return payment_processor_instance.process_payment(request)
+        payment_processor = payment_strategy()
 
+        # Process payment with booking details
+        return payment_processor.process_payment(request, booking)
     # Handle unsupported payment method
-    return render(request, 'error.html', {'error_message': 'Unsupported payment method'})
+    return render(request, 'error.html', {'error_message': 'Payment method not found'})
