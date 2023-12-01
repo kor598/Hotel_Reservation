675fcfa429b410d37d042266882257b23a5b059c
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 17:49:24 +0000

    Get booking values for payment

--- payments/payment_processors/paypal_payment_processor.py
+++ payments/payment_processors/paypal_payment_processor.py
@@ -5,19 +5,18 @@ import uuid
 from paypal.standard.forms import PayPalPaymentsForm
 from django.urls import reverse
 from django.shortcuts import render, redirect
-# from django.contrib import messages
 
 
 # concrete strategy for PayPal
 class PaypalPaymentProcessor(PaymentStrategy, View):
-# add self later
-    def process_payment(request):
+
+    def process_payment(self, request, booking):
         host = request.get_host()
 
         paypal_dict = {
             'business': settings.PAYPAL_RECEIVER_EMAIL,
-            'amount': '20.00',  # replace with order total later
-            'item_name': 'Order 1',  # replace with order number later
+            'amount': str(booking.total_amount),  # Replace with the dynamic total amount
+            'item_name': f'Booking #{booking.id}',  # Replace with dynamic booking details
             'invoice': str(uuid.uuid4()),
             'currency_code': 'USD',
             'notify_url': f'http://{host}{reverse("paypal-ipn")}',
@@ -29,14 +28,8 @@ class PaypalPaymentProcessor(PaymentStrategy, View):
         context = {'form': form}
         return render(request, 'payment.html', context)
 
-    def payment_success(request):
-        # messages.success(request, 'Your order has been successfully processed')
+    def payment_success(self, request):
         return render(request, 'success.html')
-        #return redirect(reverse('success'))
 
-
-    def payment_failure(request):
-        # messages.error(request, 'Your order has been cancelled')
+    def payment_failure(self, request):
         return render(request, 'cancelled.html')
-        # return redirect(reverse('cancelled'))
-
