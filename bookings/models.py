1786846e2097aba38c86bd5be52b11e7a96df25c
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 18:44:59 +0000

    Add total_price field to Booking model

--- bookings/models.py
+++ bookings/models.py
@@ -13,6 +13,7 @@ class Booking(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     check_in_date = models.DateTimeField()
     check_out_date = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f'{self.user} has booked {self.room} from {self.check_in_date} to {self.check_out_date}'
