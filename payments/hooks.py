b8ed7dd76e49ad800413fbc0ac5ece4af5903438
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 11:24:49 +0000

    Fix name issue

--- payments/hooks.py
+++ payments/hooks.py
@@ -1,14 +1,15 @@
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received, invalid_ipn_received
 from django.dispatch import receiver
-from models import Booking
+from models import Order
 
+#
 @receiver(valid_ipn_received)
 def valid_ipn_signal(sender, **kwargs):
     ipn_obj = sender
     if ipn_obj.payment_status == ST_PP_COMPLETED:
 
-        Booking.objects.create()
+        Order.objects.create()
 
 
 @receiver(invalid_ipn_received)
@@ -16,6 +17,4 @@ def invalid_ipn_signal(sender, **kwargs):
     ipn_obj = sender
     if ipn_obj.payment_status == ST_PP_COMPLETED:
 
-        Booking.objects.create()
-
-            
+        Order.objects.create()
