f8a35a615e7fc109eca29a7dabee7cff27479cc2
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-01 17:42:24 +0000

    Add Order model to payments

--- payments/models.py
+++ payments/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from accounts.models import User
+from bookings.models import Booking
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-class Payment(models.Model):
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10)
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} - {self.timestamp}"
