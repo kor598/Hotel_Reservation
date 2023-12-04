311fa4e682cb17191f6a19dc84fcb3145d8ccb44
adile <90270570+AdileKutlu@users.noreply.github.com>
2023-12-04 17:05:32 +0000

    Update success and failure html and logic

--- payments/payment_processors/paypal_payment_processor.py
+++ payments/payment_processors/paypal_payment_processor.py
@@ -1,3 +1,4 @@
+from bookings.models import Booking
 from .payment_strategies import PaymentStrategy
 from django.views import View
 from django.conf import settings
@@ -5,45 +6,72 @@ import uuid
 from paypal.standard.forms import PayPalPaymentsForm
 from django.urls import reverse
 from django.shortcuts import render, redirect
-from models import Order
+from payments.models import Order
 
 # concrete strategy for PayPal
 class PayPalPaymentProcessor(PaymentStrategy, View):
 
-    def process_payment(self, request, booking):
+    def get(self, request, booking_id):
+
+        payer_id = request.GET.get('PayerID')
+
+        if payer_id:
+            # Payment success logic
+            return self.payment_success(request, booking_id, payer_id)
+        else:
+            # Payment failure logic
+            return self.payment_failure(request, booking_id)
+
+    def process_payment(self, request, booking_id):
+        try:
+            booking = Booking.objects.get(id=booking_id)
+        except Booking.DoesNotExist:
+            # Handle the case where the booking is not found
+            return render(request, 'booking_not_found.html')
+
         host = request.get_host()
 
         paypal_dict = {
             'business': settings.PAYPAL_RECEIVER_EMAIL,
-            'amount': booking.total_price, 
-            'item_name': f'Booking # {booking.id}',  
+            'amount': booking.total_price,
+            'item_name': f'Booking # {booking.id}',
             'invoice': str(uuid.uuid4()),
             'currency_code': 'EUR',
             'notify_url': request.build_absolute_uri(reverse('paypal-ipn')),
-            'return_url': request.build_absolute_uri(reverse('payment_success')),
-            'cancel_return': request.build_absolute_uri(reverse('payment_failure')),
+            'return_url': request.build_absolute_uri(reverse('paypal-success', kwargs={'booking_id': booking.id})),
+            'cancel_return': request.build_absolute_uri(reverse('paypal-cancelled', kwargs={'booking_id': booking.id})),
         }
 
         form = PayPalPaymentsForm(initial=paypal_dict)
-        context = {'form': form}
+        context = {'form': form, 'booking': booking}
         return render(request, 'payment.html', context)
+    
+    def payment_success(self, request, booking_id, payer_id):
 
-    def payment_success(self, request,booking):
+        booking = Booking.objects.get(id=booking_id)
+
         order = Order.objects.create(
             user=request.user,  # Assuming the user is authenticated
             amount=booking.total_price,
             status='Paid',
             booking=booking,
+            payer_id=payer_id,  # Include payer_id in the order
         )
 
-        # Update booking payment status if needed
         booking.payment_status = 'Paid'
         booking.save()
 
-        return render(request, 'success.html')
+        return render(request, 'success.html', {'order': order, 'booking': booking})
 
-    def payment_failure(self, request, booking):
-        
+
+    def payment_failure(self, request, booking_id):
+
+        try:
+            booking = Booking.objects.get(id=booking_id)
+        except Booking.DoesNotExist:
+            # Handle the case where the booking is not found
+            return render(request, 'booking_not_found.html')
+
         order = Order.objects.create(
             user=request.user,  # Assuming the user is authenticated
             amount=booking.total_price,
@@ -51,9 +79,8 @@ class PayPalPaymentProcessor(PaymentStrategy, View):
             booking=booking,
         )
 
-        # Update booking payment status if needed
         booking.payment_status = 'Failed'
         booking.save()
 
         # Handle failure within the payment processor
-        return render(request, 'cancelled.html', {'order': order})
+        return render(request, 'cancelled.html', {'order': order, 'booking': booking})
